f31e1a4723c96aa103f918178d1c59ee00cc1715
FelipeLorini <felipelorini11@gmail.com>
2026-09-03 20:55:18 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,10 +1,16 @@
-module.exports = {
-  db: {
-    host: 'mysql-367524de-eventhubmvc.l.aivencloud.com',
-    port: 22826,
-    user: 'avnadmin',
-    password: process.env.DB_PASSWORD,
-    name: 'defaultdb',
-    ssl: true
-  }
-};
+const mysql = require('mysql2/promise');
+const env = require('./env');
+
+const pool = mysql.createPool({
+  host: env.db.host,
+  port: env.db.port,
+  user: env.db.user,
+  password: env.db.password,
+  database: env.db.name,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  ssl: env.db.ssl ? { rejectUnauthorized: false } : undefined
+});
+
+module.exports = pool;
